8c9b52bb3be4f0bcd1c17b6b2e37b6d32d2a3533
Caolan McMahon <caolan@caolanmcmahon.com>
2012-06-19 15:05:28 -0700

    update validation rules for twitter and website properties

--- ddoc/ddoc.js
+++ ddoc/ddoc.js
@@ -14,32 +14,37 @@ exports.views = {
 };
 
 exports.validate_doc_update = function (newDoc, oldDoc, userCtx) {
+    var action = oldDoc ? (newDoc._deleted ? 'remove': 'update'): 'create';
+
     if ((oldDoc && oldDoc.type === 'profile') || newDoc.type === 'profile') {
+        if (action === 'update' || action === 'create') {
+            if (newDoc.website && !/^https?:\/\//.test(newDoc.website)) {
+                throw {forbidden: 'Website must include http:// or https://'};
+            }
+            if (newDoc.twitter && !/^[A-Za-z0-9_]+$/.test(newDoc.twitter)) {
+                throw {forbidden: 'Invalid twitter username'};
+            }
+        }
         for (var i = 0; i < userCtx.roles.length; i++) {
             if (userCtx.roles[i] === '_admin') {
-                // _admin users can do anything
+                // _admin users can do anything provided it's a valid doc
                 return;
             }
         }
         if (!userCtx.name) {
             throw {unauthorized: 'You must be logged in'};
         }
-        if (oldDoc) {
-            if (newDoc._deleted) {
-                // remove
-                if (userCtx.name !== oldDoc.name) {
-                    throw {unauthorized: 'Only the owner can remove a profile'};
-                }
-            }
-            else {
-                // update
-                if (userCtx.name !== oldDoc.name) {
-                    throw {unauthorized: 'Only the owner can update a profile'};
-                }
+        if (action === 'remove') {
+            if (userCtx.name !== oldDoc.name) {
+                throw {unauthorized: 'Only the owner can remove a profile'};
             }
         }
-        else {
-            // create
+        else if (action === 'update') {
+            if (userCtx.name !== oldDoc.name) {
+                throw {unauthorized: 'Only the owner can update a profile'};
+            }
+        }
+        else if (action === 'create') {
             if (userCtx.name !== newDoc.name) {
                 throw {unauthorized: 'profile.name must match your username'};
             }
